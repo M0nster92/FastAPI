7b6feb1ed052397e6dc601f89873b9f3d95a2b54
M0nster92 <fahimalsafi@gmail.com>
2022-08-28 09:48:34 -0400

    sync

--- init_project/db/models.py
+++ init_project/db/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.sql.sqltypes import Integer, String, Boolean
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column
+from sqlalchemy.sql.schema import ForeignKey
 from db.database import Base
 
 class DbUser(Base):
@@ -9,3 +11,14 @@ class DbUser(Base):
     username = Column(String)
     email = Column(String)
     password = Column(String)
+    items = relationship('DbArticle', back_populates='user')
+
+class DbArticle(Base):
+    __tablename__ = 'articles'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    published = Column(Boolean)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('DbUser', back_populates='items')
--- init_project/schemas.py
+++ init_project/schemas.py
@@ -1,3 +1,4 @@
+from typing import List
 from pydantic import BaseModel
 
 class UserBase(BaseModel):
@@ -5,8 +6,16 @@ class UserBase(BaseModel):
     email: str
     password: str
 
+class Article(BaseModel):
+    title: str
+    content : str
+    published: bool
+    class Config():
+        orm_mode = True
+
 class UserDisplay(BaseModel):
     username: str
     email: str
+    items : List[Article] = []
     class Config():
         orm_mode = True
